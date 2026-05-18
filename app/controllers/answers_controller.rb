155c3ebc6ea1feb831b332bceb7503f549bf593d
Alexandre Theol <alexandre.theol@gmail.com>
2026-05-18 12:20:00 +0200

    rails stupid coaching

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+class AnswersController < ApplicationController
+  def respond
+    @question = params["question"]
+    if @question == "I am going to work"
+      then
+      @answer = "Great"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
